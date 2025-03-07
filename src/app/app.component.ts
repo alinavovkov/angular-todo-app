2001fd749feba14c10480a6fecd8de8c3496d8f3
alinavovkov <alinvovkov@gmail.com>
2025-03-07 09:06:56 +0200

    basic functional was added(creating, deleting task)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './types/todo';
+
+const todos = [
+  { id: 1, title: 'HTML + CSS', completed: true },
+  { id: 2, title: 'JS', completed: false },
+  { id: 3, title: 'React', completed: false },
+  { id: 4, title: 'Vue.js', completed: false },
+
+];
 
 @Component({
   selector: 'app-root',
@@ -7,5 +17,35 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+      Validators.minLength(3),
+      ],
+  }),
+  });
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+  get activeTodos() {
+    return this.todos.filter(todo => !todo.completed);
+  }
+  addTodo() {
+    if (this.todoForm.invalid) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title.value,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
